bc4e6e46abecdbb9ce2eff8b204f2646380c5bbf
Daniel Baumgarten <incognym@googlemail.com>
2018-12-11 12:46:38 +0100

    Verbesserte Analysepräzision + emoji-analyse. Closes #27

--- analyzer/analyzer.py
+++ analyzer/analyzer.py
@@ -3,7 +3,7 @@ from kafka import KafkaProducer
 import ssl
 import time
 import sys
-from sentimentanalysis import *
+from sentimentanalysis import get_tweet_sentiment
 from convertcerts import pkcs12_to_pem
 import argparse
 import json
@@ -12,16 +12,18 @@ from pymongo.errors import DuplicateKeyError, NotMasterError
 from pymongo import IndexModel, ASCENDING
 import datetime
 
-group_id = "analyzers" 
+group_id = "analyzers"
 
 """
 Quick and dirty way to read java propertie files as pyjavaproperties seems to be broken.
 Will probably break on non-trivial config files.
 """
+
+
 def read_config(file):
     config = {}
     for line in file:
-        if len(line.strip()) > 0 and not line.strip().startswith("#"): 
+        if len(line.strip()) > 0 and not line.strip().startswith("#"):
             key, value = line.split("=")
             key = key.strip()
             value = value.strip()
@@ -37,32 +39,39 @@ def read_config(file):
     if "bootstrap.servers" not in config:
         config["bootstrap.servers"] = "kafka:9092"
 
-
     return config
+
 
 """
 Assuming the default certificate location and given a password to decrypt them this function sets up and returns an sslcontext.
 During this the certificates are converted (copied to the same dir, but with another file-extension) to an appropriate format.
 """
+
+
 def create_sslcontext(password):
     def pwfunc():
         return password
 
-    #convert pkcs12 certs to pem
-    pkcs12_to_pem("../certificates/kafka.client.keystore.jks",password)
-    pkcs12_to_pem("../certificates/kafka.client.truststore.jks",password)
+    # convert pkcs12 certs to pem
+    pkcs12_to_pem("../certificates/kafka.client.keystore.jks", password)
+    pkcs12_to_pem("../certificates/kafka.client.truststore.jks", password)
 
     ctx = ssl.create_default_context()
-    ctx.load_verify_locations(cafile="../certificates/kafka.client.truststore.pem")
-    ctx.load_cert_chain(certfile="../certificates/kafka.client.keystore.pem",password=pwfunc)
+    ctx.load_verify_locations(
+        cafile="../certificates/kafka.client.truststore.pem")
+    ctx.load_cert_chain(
+        certfile="../certificates/kafka.client.keystore.pem", password=pwfunc)
     return ctx
+
 
 """
 Subscribe a list of topics and wait until the assignment of partitions to the consumer is completed.
 If message is not None it will be printed every second while waiting
 After some unsuccessfull waits for assignments the subscription is retired
 """
-def subscribe_and_wait_for_assignment(consumer,topics,message=None):
+
+
+def subscribe_and_wait_for_assignment(consumer, topics, message=None):
     count = 0
     consumer.subscribe(topics=topics)
     consumer.poll(0)
@@ -78,26 +87,27 @@ def subscribe_and_wait_for_assignment(consumer,topics,message=None):
             consumer.poll(0)
             count = 0
 
+
 """"
 Given a mongodb collection this function creates indexes that will be usefull for later queries
 """
+
+
 def create_indexes(collection):
     while True:
         try:
-            id = IndexModel([("id",ASCENDING)],unique=True)
-            hashtags = IndexModel([("hashtags",ASCENDING)])
-            created = IndexModel([("created",ASCENDING)])
-            collection.create_indexes([id,hashtags,created])
+            id = IndexModel([("id", ASCENDING)], unique=True)
+            hashtags = IndexModel([("hashtags", ASCENDING)])
+            created = IndexModel([("created", ASCENDING)])
+            collection.create_indexes([id, hashtags, created])
         except NotMasterError:
             print("Waiting for mongodb-server to become primary...")
             time.sleep(1)
             continue
         break
 
-    
 
-
-def main(input_topic,db,group_id=group_id,seek=False,stop_event=None,start_event=None,debug=False):
+def main(input_topic, db, group_id=group_id, seek=False, stop_event=None, start_event=None, debug=False):
     conf = read_config(open("../config.properties"))
 
     security_protocol = conf.get("security.protocol")
@@ -112,11 +122,11 @@ def main(input_topic,db,group_id=group_id,seek=False,stop_event=None,start_event
     while consumer == None:
         try:
             consumer = KafkaConsumer(
-            bootstrap_servers=conf["bootstrap.servers"],
-            group_id=group_id,
-            security_protocol=security_protocol,
-            ssl_context=sslctx,
-            consumer_timeout_ms= 1000
+                bootstrap_servers=conf["bootstrap.servers"],
+                group_id=group_id,
+                security_protocol=security_protocol,
+                ssl_context=sslctx,
+                consumer_timeout_ms=1000
             )
         except errors.NoBrokersAvailable:
             print("Kafka unreachable. Retrying...")
@@ -127,9 +137,6 @@ def main(input_topic,db,group_id=group_id,seek=False,stop_event=None,start_event
             time.sleep(3)
     print("Connected to kafka!")
 
-
-
-
     db_name, collection_name = db.split("/")
 
     dbclient = MongoClient(conf["mongodb"])
@@ -138,7 +145,8 @@ def main(input_topic,db,group_id=group_id,seek=False,stop_event=None,start_event
 
     create_indexes(tweetcollection)
 
-    subscribe_and_wait_for_assignment(consumer,(input_topic,),"Analyzer waiting for assignments")
+    subscribe_and_wait_for_assignment(
+        consumer, (input_topic,), "Analyzer waiting for assignments")
 
     if seek == "begin":
         consumer.seek_to_beginning()
@@ -155,22 +163,34 @@ def main(input_topic,db,group_id=group_id,seek=False,stop_event=None,start_event
                 tweet = json.loads(msg.value.decode())
             except json.JSONDecodeError:
                 if debug:
-                    print("Warning:",msg.value.decode(),"is no valid json")
+                    print("Warning:", msg.value.decode(), "is no valid json")
                 continue
+
+            tweet["isRated"] = True
+            tweet["createdAt"] = datetime.datetime.strptime(
+                tweet["createdAt"], "%b %d, %Y %I:%M:%S %p")
+
             sentiment = get_tweet_sentiment(tweet["text"])
-            tweet["rating"]=sentiment
-            tweet["isRated"]=True
-            tweet["createdAt"]=datetime.datetime.strptime(tweet["createdAt"],"%b %d, %Y %I:%M:%S %p")
+
+            if sentiment != None:
+                tweet["rating"] = sentiment
+                tweet["isRated"] = True
+            else:
+                tweet["rating"] = 0.0
+                tweet["isRated"] = False
+                if debug:
+                    print("Warning: Could not detect language of:", tweet[id])
+
             try:
                 tweetcollection.insert_one(tweet)
             except DuplicateKeyError:
                 # There is already a tweet with this id in the db. Nothing left to do, go on.
                 if debug:
-                    print ("Ignoring duplicate:",tweet["id"])
+                    print("Ignoring duplicate:", tweet["id"])
                 pass
 
             if debug:
-                print ("From:",msg.topic," : ",tweet)
+                print("From:", msg.topic, " : ", tweet)
             if stop_event:
                 if stop_event.isSet():
                     break
@@ -178,14 +198,20 @@ def main(input_topic,db,group_id=group_id,seek=False,stop_event=None,start_event
             if stop_event.isSet():
                 break
 
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Analyze messages from kafka')
-    parser.add_argument('--input_topic', type=str, help='The kafka topic to load raw tweets from', default="tweets")
-    parser.add_argument('--db', type=str, help='Name of the database and collection in which to store analyzed tweets', default="twitter/tweets")
-    parser.add_argument('--group_id', type=str, help='The if of the consumer-group this consumer belongs to', default=group_id)
-    parser.add_argument('--seek', type=str, help='Seek to "begin", "end" or "none" when starting up.', default="none")
-    parser.add_argument('--debug', type=bool, help='Print debug infos (like analyzed messages)', default=False)
-    
+    parser.add_argument('--input_topic', type=str,
+                        help='The kafka topic to load raw tweets from', default="tweets")
+    parser.add_argument(
+        '--db', type=str, help='Name of the database and collection in which to store analyzed tweets', default="twitter/tweets")
+    parser.add_argument('--group_id', type=str,
+                        help='The if of the consumer-group this consumer belongs to', default=group_id)
+    parser.add_argument(
+        '--seek', type=str, help='Seek to "begin", "end" or "none" when starting up.', default="none")
+    parser.add_argument(
+        '--debug', type=bool, help='Print debug infos (like analyzed messages)', default=False)
+
     args = parser.parse_args()
     print(args)
 
--- analyzer/sentimentanalysis.py
+++ analyzer/sentimentanalysis.py
@@ -1,6 +1,9 @@
 import re
 from textblob import TextBlob
 from textblob_de import TextBlobDE
+from langdetect import detect
+from langdetect.lang_detect_exception import LangDetectException
+from emoji import analyze_emoji_sentimens
 
 
 def clean_tweet(tweet):
@@ -11,7 +14,35 @@ def clean_tweet(tweet):
     return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t]) | (\w+:\ / \ / \S+)", " ", tweet).split())
 
 
-def get_tweet_sentiment(tweet):
+def get_tweet_sentiment(tweet, analyze_emojis=True):
+    """
+    Auto-detect the tweets-language and classify sentiment of passed tweet
+    using textblob's sentiment method.
+    If the language is neither german nor english (or could not be auto-detected) it returns None.
+    If analyze_emojis is True the sentiment of the emojis in the text is also computed. Sentiments of text and emojis is summed (and limited to [-1,1])
+    """
+    text_sentiment = None
+    try:
+        lang = detect(clean_tweet(tweet))
+        if lang == "de":
+            text_sentiment = get_de_tweet_sentiment(tweet)
+        elif lang == "en":
+            text_sentiment = get_en_tweet_sentiment(tweet)
+    except LangDetectException:
+        pass
+
+    if not analyze_emojis:
+        return text_sentiment
+
+    emoji_sentiment = analyze_emoji_sentimens(tweet)
+
+    if text_sentiment == None:
+        return emoji_sentiment
+
+    return max(min(emoji_sentiment+text_sentiment, 1), -1)
+
+
+def get_en_tweet_sentiment(tweet):
     '''
     Utility function to classify sentiment of passed tweet
     using textblob's sentiment method
@@ -20,6 +51,7 @@ def get_tweet_sentiment(tweet):
     analysis = TextBlob(clean_tweet(tweet))
     # set sentiment
     return analysis.sentiment.polarity
+
 
 def get_de_tweet_sentiment(tweet):
     '''
@@ -35,21 +67,21 @@ def get_de_tweet_sentiment(tweet):
 if __name__ == '__main__':
     print('-- en tweets --')
     en_tweets = ['I really like the weather today!', 'This movie was so boring...',
-              'Found today a very interesting book!']
+                 'Found today a very interesting book!']
     for t in en_tweets:
         print(t + ' -> ' + get_tweet_sentiment(t))
 
     print('-- de tweets --')
-    de_tweets = ['Das Wetter ist fantastisch heute!', 
-				 'Der Film war langweilig...',
+    de_tweets = ['Das Wetter ist fantastisch heute!',
+                 'Der Film war langweilig...',
                  'Gerade ein sehr interessantes Buch entdeckt.',
-				 'Scheisse ist das geil.',
-				 'Verschissener Kackrotz bei der Bahn!',
-				 'Da musste ich am bloeden Knopf rumfingern, das mir der Humpen umgekippt ist! Sapperlot.',
-				 'Es erschien ein Engel und zwitscherte ein Lied',
-				 'Holla die Waldfee, der Hopfensmoothie laesst mich voll abgehen bei dem Tindergarten. Yolo.',
-				 'Ahnma! Die Chixen im Beef sind voll cheedo!',
-				 'Unglaublich! Die Frauen im Club sind so toll!']
+                 'Scheisse ist das geil.',
+                 'Verschissener Kackrotz bei der Bahn!',
+                 'Da musste ich am bloeden Knopf rumfingern, das mir der Humpen umgekippt ist! Sapperlot.',
+                 'Es erschien ein Engel und zwitscherte ein Lied',
+                 'Holla die Waldfee, der Hopfensmoothie laesst mich voll abgehen bei dem Tindergarten. Yolo.',
+                 'Ahnma! Die Chixen im Beef sind voll cheedo!',
+                 'Unglaublich! Die Frauen im Club sind so toll!']
     for t in de_tweets:
         print(t + ' -> ' + get_de_tweet_sentiment(t))
 
